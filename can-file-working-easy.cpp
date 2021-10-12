e567c4b755b654ae3e4a4b5a84962ac25a5f533d
nicolabernardi <bern.nico29@gmail.com>
2021-10-13 00:35:13 +0200

    backup tioccà

--- can-file-working-easy.cpp
+++ can-file-working-easy.cpp
@@ -13,7 +13,8 @@
 #define MAX_DATA_BYTE 8
 #define LOG_LENGTH_BYTE 1000
 
-int s, file;
+int s;
+FILE *file = NULL;
 bool loop = true;
 char buffer[MAX_DATA_BYTE*2]; //each byte is represented with 2 hex symbols, so 2 chars
 char log_row_buffer[LOG_LENGTH_BYTE];
@@ -21,7 +22,7 @@ char log_row_buffer[LOG_LENGTH_BYTE];
 void quit_handler(int signal){
     loop = false;
     if(s>0) close(s);
-    if(file>0) close(file);
+    if(file != NULL) fclose(file);
 }
 
 int main()
@@ -33,7 +34,7 @@ int main()
     signal(SIGTSTP, quit_handler);
 
     s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
-    file = open("can.log", O_WRONLY|O_APPEND);
+    file = fopen("can.log", "a");
     if (s < 0 || file < 0)
     {
         perror("error");
@@ -51,7 +52,6 @@ int main()
         addr.can_ifindex = ifr.ifr_ifindex;
         if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
             perror("Bind");
-            printf("i am here\n");
             return 1;
         }
 
@@ -82,23 +82,10 @@ int main()
 
             //printf("canid = %03X - ", frame.can_id);
             
-            //memset(log_row_buffer, 0, LOG_LENGTH_BYTE);
-            sprintf(log_row_buffer, "(%ld.%ld) %s %03X#%s\n", tv.tv_sec, tv.tv_usec, ifr.ifr_name, frame.can_id, buffer);
-            //sprintf(log_row_buffer, "%s\n", buffer);
-            printf("(%ld.%ld) %s %03X#%s\n", tv.tv_sec, tv.tv_usec, ifr.ifr_name, frame.can_id, buffer);
-            //printf("(%ld.%ld) %s %03X#%s\n", tv.tv_sec, tv.tv_usec, ifr.ifr_name, frame.can_id, buffer);
-           
-            //write(file, log_row_buffer, strlen(log_row_buffer));
-            
-            //printf("%s\n", log_row_buffer);
+
+            fprintf(file, "(%ld.%06ld) %s %03X#%s\n", tv.tv_sec, tv.tv_usec, ifr.ifr_name, frame.can_id, buffer);
+
             fflush(stdout);
-            sleep(1);
-            //printf("sto cazzo vai a capo\n");
-
-            
-            //printf("received at %ld.%ld\n", tv.tv_sec, tv.tv_usec*1000000);
-
-            //printf("data: %s", frame.data);
         }
 
         //EagleTelemetry::CANMessage message;
