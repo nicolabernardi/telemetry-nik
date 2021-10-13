6eefb42d98f7a7e4727822ad4279de868f7e8411
nicolabernardi <bern.nico29@gmail.com>
2021-10-14 00:35:19 +0200

    pulizia

--- can-service-file-descriptors.cpp
+++ can-service-file-descriptors.cpp
@@ -1,5 +1,6 @@
 #include <linux/can.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <net/if.h>
 #include <string.h>
 #include <sys/ioctl.h>
@@ -11,17 +12,18 @@
 #include <signal.h>
 
 #define MAX_DATA_BYTE 8
-#define LOG_LENGTH_BYTE 100
+#define MAX_LOG_LENGTH_BYTE 100
 
 int s, file;
 bool loop = true;
-char buffer[MAX_DATA_BYTE*2+1]; //each byte is represented with 2 hex symbols, so 2 chars + string terminator
-char log_row_buffer[LOG_LENGTH_BYTE];
+char buffer[MAX_DATA_BYTE*2+1]; //each byte is represented with 2 hex symbols, so the length is #byte*2 chars + string terminator
+char log_row_buffer[MAX_LOG_LENGTH_BYTE];
 
 void quit_handler(int signal){
     loop = false;
     if(s>0) close(s);
     if(file>0) close(file);
+    exit(0);
 }
 
 int main()
@@ -54,7 +56,6 @@ int main()
         addr.can_ifindex = ifr.ifr_ifindex;
         if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
             perror("Bind");
-            printf("i am here\n");
             return 1;
         }
 
@@ -64,14 +65,14 @@ int main()
         while(loop){
             nbytes = read(s, &frame, sizeof(struct can_frame));
             if (nbytes < 0) {
-                fprintf(stderr, "Read error: nothing to read\n");
-                return 1;
+                fprintf(stderr, "Read error: read less than 0 bytes\n");
+                return 2;
             }
 
             if (nbytes < sizeof(struct can_frame))
             {
-                fprintf(stderr, "read: incomplete CAN frame\n");
-                return 1;
+                fprintf(stderr, "Read error: incomplete CAN frame\n");
+                return 3;
             }
 
             struct timeval tv;
@@ -84,41 +85,26 @@ int main()
             }
             buffer[frame.can_dlc*2+1] = 0; //string terminator
 
-            //printf("canid = %03X - ", frame.can_id);
-            
-            //memset(log_row_buffer, 0, LOG_LENGTH_BYTE);
-            snprintf(log_row_buffer, LOG_LENGTH_BYTE, "(%ld.%06ld) %s %03X#%s\n", tv.tv_sec, tv.tv_usec, ifr.ifr_name, frame.can_id, buffer);
-            //sprintf(log_row_buffer, "%s\n", buffer);
-            //printf("lunghezza buffer: %ld\n", strlen(log_row_buffer));
-            //printf("(%ld.%ld) %s %03X#%s\n", tv.tv_sec, tv.tv_usec, ifr.ifr_name, frame.can_id, buffer);
-            //printf("(%ld.%ld) %s %03X#%s\n", tv.tv_sec, tv.tv_usec, ifr.ifr_name, frame.can_id, buffer);
-           
-            lseek(file, 0, SEEK_END); //posizionati alla fine per scrivere
+            //write to file
+            snprintf(log_row_buffer, MAX_LOG_LENGTH_BYTE, "(%ld.%06ld) %s %03X#%s\n", tv.tv_sec, tv.tv_usec, ifr.ifr_name, frame.can_id, buffer);
             write(file, log_row_buffer, strlen(log_row_buffer));
             
             //printf("%s", log_row_buffer);
-            //fflush(stdout);
-            //printf("sto cazzo vai a capo\n");
 
             counter++;
 
+            //update file every 500 messages
             if(counter == 500){
                 counter = 0;
                 close(file);
                 printf("closed file, now reopen\n");
                 file = open("can.log", O_WRONLY|O_APPEND);
+                if (file < 0)
+                {
+                    perror("error");
+                }
             }
-
-
-
-            //printf("received at %ld.%ld\n", tv.tv_sec, tv.tv_usec*1000000);
-
-            //printf("data: %s", frame.data);
         }
-
-        //EagleTelemetry::CANMessage message;
-        //message.setId(frame.can_id);
-        
     }
-    return 0;
+    return 0; //not really useful
 }
