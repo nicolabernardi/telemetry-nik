2bc0b864bf07bb65aa7b935afbff969082e8f411
Lorenzo <lorenzo.rossi-4@studenti.unitn.it>
2021-11-21 19:54:52 +0100

    Minor changes

--- server2.py
+++ server2.py
@@ -10,6 +10,67 @@ from utils.Parser import Parser
 
 sel = selectors.DefaultSelector()
 
+def getArrayFromMessage(data):
+    msg = data.replace("b", "").replace("'", "") # remove b''
+    msg = msg.strip('][').split(',')
+
+    return msg
+
+def getData(tuple):
+    # get the timestamp
+    timestamp = float(tuple[0])
+
+    # id#msg
+    canMsg = tuple[1].split("#")
+
+    # id
+    id = int(canMsg[0], 16)
+
+    msg = canMsg[1]
+
+    return timestamp, id, msg
+
+
+def readData(data, addr):
+    ## CAN MESSAGE TO BE PARSED
+    print(repr(data))
+    # b'[(1636363447.371728; 0C0#022AAC0000000000), (...; ...), ...]''
+    message = getArrayFromMessage(repr(data))
+    print("SIZE:", len(message))
+
+    file = "can-server.log"
+    f = open(file, "a")
+
+    # [(1636363447.371728; 0C0#022AAC0000000000), (...; ...), ...]
+    for pair in message:
+        tuple = pair.strip(')(').split(';')
+        # ["ts", "id#msg"]
+
+        timestamp, id, msg = getData(tuple)
+
+        # padding
+        #while(len(can[1]) < 16):
+            #can[1] = "00" + can[1]
+
+        f.write("{:.6f}\t".format(timestamp) + str(id) + "#" + msg + "\n")
+
+        # list with hex converted to int
+        n = 2
+        print(msg)
+        msg = [int(msg[i:(i + n)], 16) for i in range(0, len(msg), n)]
+
+        #print(timestamp, " - ", id, "#", msg)
+        p = Parser()
+        l_obj = p.parseMessage(timestamp, id, msg)
+        if(l_obj): # list not empty
+            for obj in l_obj:
+                print("Echoing ", obj.type, "informations:", addr)
+                dict = obj.get_dict()
+                for k, v in dict.items():
+                    print("-", k, ": ", v)
+
+    f.close()
+
 
 def accept_wrapper(sock):
     conn, addr = sock.accept()  # Should be ready to read
@@ -24,7 +85,11 @@ def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
     N_MESSAGES = 16
-    CAN_MESSAGE_LEN = 1 + (41 + 2) * N_MESSAGES + 1 + 3
+    N_CHARS_PER_MESSAGE = 42
+    # 3 indicates "b''"
+    # 1 is the "[", the "]" is not necessary, because it's counted one comma
+    # more in N_CHARS_PER_MESSAGE
+    CAN_MESSAGE_LEN = 3 + 1 + (N_CHARS_PER_MESSAGE * N_MESSAGES)
 
     try:
         if mask & selectors.EVENT_READ:
@@ -38,53 +103,8 @@ def service_connection(key, mask):
 
         if mask & selectors.EVENT_WRITE:
             if data.outb:
-                ## CAN MESSAGE TO BE PARSED
-                print(repr(data.outb))
-                # b'[(1636363447.371728; 0C0#022AAC0000000000), (...; ...), ...]''
-                message = repr(data.outb).replace("b", "").replace("'", "") # remove b''
-                message = message.strip('][').split(',')
-                print("SIZE:", len(message))
+                readData(data.outb, data.addr)
 
-                file = "can.log"
-                f = open(file, "a")
-
-                # [(1636363447.371728; 0C0#022AAC0000000000), (...; ...), ...]
-                for pair in message:
-                    tuple = pair.strip(')(').split(';')
-                    # ["ts", "id#msg"]
-
-                    # get the timestamp
-                    timestamp = float(tuple[0])
-
-                    # id#msg
-                    can = tuple[1].split("#")
-
-                    # padding
-                    #while(len(can[1]) < 16):
-                        #can[1] = "00" + can[1]
-
-                    f.write("(" + tuple[0] + ")\t" + can[0] + "#" + can[1] + "\n")
-
-                    # id
-                    id = int(can[0], 16)
-
-                    # list with hex converted to int
-                    n = 2
-                    line = can[1]
-                    print(line)
-                    msg = [int(line[i:(i + n)], 16) for i in range(0, len(line), n)]
-
-                    #print(timestamp, " - ", id, "#", msg)
-                    p = Parser()
-                    l_obj = p.parseMessage(timestamp, id, msg)
-                    if(l_obj): # list not empty
-                        for obj in l_obj:
-                            print("Echoing ", obj.type, "informations:", data.addr)
-                            dict = obj.get_dict()
-                            for k, v in dict.items():
-                                print("-", k, ": ", v)
-
-                f.close()
                 sent = sock.send(data.outb)  # Should be ready to write
                 data.outb = data.outb[sent:]
     ## ERROR CATCHING
